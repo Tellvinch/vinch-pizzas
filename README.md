cb718e4cf5043649c748f5e84c7a3aa2027b2576
Tellvinch <tellvinchimani@gmail.com>
2020-04-19 15:13:04 +0300

    add changes

--- README.md
+++ README.md
@@ -1,20 +1,33 @@
 # VINCH PIZZAS
+
 #### An application for Vinch Pizzas that allows customers to order pizzas online, 19th April 2020
+
 #### By **Tellvinch imani**
+
 ## Description
+
 This a web application for vinch pizzas.It allows customers to order pizzas at the comfort of their home.It is a highly graded website.
-Its  attractive and one of the best works so far.
+Its attractive and one of the best works so far.
+
 ## Setup/Installation Requirements
-* Click  on this [link](https://tellvinch.github.io/vinch-pizzas/) to open the application.
-* Scroll down to read more.
-* Place your order and choose if you want it to be delivered or not.
+
+- Click on this [link](https://tellvinch.github.io/vinch-pizzas/) to open the application.
+- Scroll down to read more.
+- Place your order and choose if you want it to be delivered or not.
 
 ## Known Bugs
+
 There are no known bugs in this application.
+
 ## Technologies Used
+
 The project uses HTML,CSS JAVASCRIPT and JQuery.
+
 ## Support and contact details
+
 For any more information contact me at tellvinchimani@gmail.com
+
 ### License
- <a href="Licence.md">MIT LICENSE</a>
+
+<a href="Licence.md">MIT LICENSE</a>
 Copyright (c) 2020 **Vinch pizzas**
--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,41 @@
+$(document).ready(function () {
+  function pizza(size, crust, toppings) {
+    this.size = size;
+    this.crust = crust;
+    this.toppings = toppings;
+  }
+  $("select.size").change(function () {
+    var size = $("option:selected").val();
+  });
+  var selectedCrust = $("select.crust").change(function () {
+    var selected = $("option:selected").val();
+    alert("Thanks for choosing your crust.The cost would be ksh 100/=");
+  });
+  var checkedTopping = $("form.toppings").change(function () {
+    var checked = $("input:checked").val();
+    alert("Thanks for choosing your topping.The cost would be ksh 100/= ");
+  });
+  var number = $("input#quantity").change(function () {
+    var quantity = $("input#quantity").val();
+  });
+  $("#submit").click(function (event) {
+    alert(
+      "Done! your order has been placed.You've ordered 1 large crispy pizza with mushrooms for toppings "
+    ); //just to see if its working
+  });
+  var deliverOrder = $("form#deliver").change(function () {
+    $("input:checked").val();
+    if ($("input:checked").val() === "yes") {
+      prompt("Enter the adress you want your order to be delivered to");
+      alert("Your delivering charges will be Sh 55/=");
+      confirm("Do you still want the delivery to be made?");
+    }
+  });
 
-$(document).ready(function() {
-       function pizza(size, crust, toppings) {
-           this.size = size;
-           this.crust = crust;
-           this.toppings = toppings;
-        } 
-        var selectedSize=$("select.size").change(function(){
-           var size=$("option:selected").val();
-           
-        })
-        var selectedCrust=$("select.crust").change(function(){
-            var selected=$("option:selected").val();
-            alert("Thanks for choosing your crust.The cost would be ksh 100/=");
-        })
-        var checkedTopping=$("form.toppings").change(function(){
-             var checked=$("input:checked").val();
-             alert("Thanks for choosing your topping.The cost would be ksh 100/=");
-            })
-        var number=$("input#quantity").change(function(){
-            var quantity= $("input#quantity").val();
-            
-        })
-        $("#submit").click(function(event){
-            alert("Done! your order has been placed.Youve ordered 1 large crispy pizza with mushrooms for toppings");//just to see if its working
-        })
-        var deliverOrder=$("form#deliver").change(function(){
-            $("input:checked").val();
-            if($("input:checked").val()=== "yes"){
-                prompt("Enter the adress you want your order to be delivered to");
-                alert("Your delivering charges will be ksh 55/=")
-                confirm("Do you still want the delivery to be made?")
-            
-            }
-        })    
-       
-        var cost=$("#cost").click(function(event){
-            alert("Hey! Customer this is your total cost "+ $("input#quantity").val()*1000);
-        })
-          
-        
-       
-        
-        
-      
+  var cost = $("#cost").click(function (event) {
+    alert(
+      "Hey! Customer this is your total cost " +
+        $("input#quantity").val() * 1000
+    );
+  });
 });
-        
-    
