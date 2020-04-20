85f99aba16be04e26363ded1c61c139fe6b2f025
Tellvinch <tellvinchimani@gmail.com>
2020-04-20 09:26:36 +0300

    add new changes

--- js/scripts.js
+++ js/scripts.js
@@ -4,37 +4,54 @@ $(document).ready(function () {
     this.crust = crust;
     this.toppings = toppings;
   }
-  pizza.prototype.order = function() {
-    return this.size +this.crust ;
-  }
+  pizza.prototype.order = function () {
+    return this.size + this.crust;
+  };
   $("button#submit").click(function (event) {
     event.preventDefault();
+    var prices = {
+      size: {
+        small: 450,
+        medium: 550,
+        large: 650,
+        extralarge: 900,
+      },
+      crust: {
+        crispy: 100,
+        stuffy: 70,
+        glutenfree: 60,
+      },
+    };
     var size = $("select#size option:checked").val();
     var crust = $("select#crust option:selected").val();
     var topping = $("form#toppings input:checked").val();
     var number = $("input#quantity").val();
-    var customersOrder = "You have ordered " + number + " " + size + " sized "+ crust + " pizza(s) with " + topping + " as topping"
-    
-
+    var customersOrder =
+      "You have ordered " +
+      number +
+      " " +
+      size +
+      " sized " +
+      crust +
+      " pizza(s) with " +
+      topping +
+      " as topping";
 
     Swal.fire(customersOrder);
     Swal.fire(deliverOrder);
   });
   $("button#cost").click(function (event) {
-    var total= "Hello Customer! this is your total cost " + $("input#quantity").val() * 1000 
-      Swal.fire(total)
-         
-  })
-  $("form#deliver ").change(function(){
-      var deliver =$("input:checked").val();
-      if($("input:checked").val() === "yes"){
-        prompt("Enter your adress");
-        Swal.fire("The delivery cost to your location is KSH 55/=")
-      }
-   })
-  
+    var total =
+      "Hello Customer! this is your total cost " +
+      $("input#quantity").val() * 1000;
+    Swal.fire(total);
+  });
+  $("form#deliver ").change(function () {
+    var deliver = $("input:checked").val();
+    if ($("input:checked").val() === "yes") {
+      prompt("Enter your adress");
+      Swal.fire("The delivery cost to your location is KSH 55/=");
+    }
+  });
 });
 
-
-
-// var deliverOrder = $("form#deliver input:checked").val();
